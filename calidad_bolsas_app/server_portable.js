84d7b827f120ad8121ae50ed839738a53ed1be10
mpmh1511-netizen <mpmh1511@gmail.com>
2026-09-26 12:00:12 -0600

    Add files via upload

--- calidad_bolsas_app/server_portable.js
+++ calidad_bolsas_app/server_portable.js
@@ -56,7 +56,9 @@ function writeDb(db) {
 
 const server = http.createServer(async (req, res) => {
   // Configurar CORS y anti-caché
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  const origin = req.headers.origin || '*';
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
